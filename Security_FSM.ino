53fea67e068821f05c71be4cf4af85ede09e58ff
Frank Milburn <fmilburn3@users.noreply.github.com>
2017-01-21 13:20:49 -0800

    Complete rewrite

---
+++ Security_FSM.ino
@@ -0,0 +1,18 @@
+
+#include "Security.h"
+//                                                 Yellow
+//               |---- Energia Pin Numbers ----|   Pause
+//               alarm  clear  green  yellow red   (mSec)
+Security zone_1 (2,     3,     4,     5,     6,    4000); 
+Security zone_2 (7,     8,     9,     10,    11,   2000);
+Security zone_3 (12,    13,    14,    15,    19,   3000);
+
+void setup() {
+  // Nothing needed here.
+}
+
+void loop() {
+  zone_1.Update();
+  zone_2.Update();
+  zone_3.Update();
+}
