31ab376d982e1b78fd1b54b59015668f23d67782
Frank Milburn <fmilburn3@users.noreply.github.com>
2017-01-23 19:14:38 -0800

    Update Security.cpp

--- Security.cpp
+++ Security.cpp
@@ -1,22 +1,22 @@
 #include "Energia.h"
 #include "Security.h"
 
-struct Fsm {                   // basic FSM structure for a single state.
-  SecurityStates nextState[4]; // next state for NONE, ALARM, BOTH, and RESET
-  unsigned long pause;         // pause before moving to next state (mSec)
+struct Fsm {                    // basic FSM structure for a single state.
+  SecurityStates nextState[3];  // next state for TIMEOUT, ALARM, and RESET
+  unsigned long timeout;        // time before moving to next state (mSec)
 };
 // Initialized FSM table for all states.
 static Fsm fsmTable[] = {
-  // ---------- nextState ----------
-  // NONE     ALARM   BOTH    RESET    pauseTime           state
-  // -------  ------  ------  ------   ---------           -------
-  {{ GREEN,   YELLOW, GREEN,  GREEN }, 0        },      // GREEN
-  {{ RED,     RED,    GREEN,  GREEN }, 0        },      // YELLOW
-  {{ RED,     RED,    GREEN,  GREEN }, 0        },      // RED
+  // ------ nextState ------
+  // TIMEOUT  ALARM   RESET    timeout             state
+  // -------  ------  ------   ---------           -------
+  {{ GREEN,   YELLOW, GREEN }, 0        },      // GREEN
+  {{ RED,     RED,    GREEN }, 0        },      // YELLOW
+  {{ RED,     RED,    GREEN }, 0        },      // RED
 };
 
 Security::Security(int alarmPin, int resetPin, int greenPin, int yellowPin,
-                   int redPin, unsigned long yellowPause)
+                   int redPin, unsigned long yellowTimeout)
 {
   greenPin_ =  greenPin;
   yellowPin_ = yellowPin;
@@ -24,8 +24,8 @@ Security::Security(int alarmPin, int resetPin, int greenPin, int yellowPin,
   alarmPin_ =  alarmPin;
   resetPin_ =  resetPin;
 
-  // User defines the yellow -- > red transition pause, all others are left at 0
-  fsmTable[YELLOW].pause = yellowPause;         
+  // User defines the yellow -- > red transition timeout, all others are left at 0
+  fsmTable[YELLOW].timeout = yellowTimeout;         
 
   // Initialize alarm inputs.
   pinMode(alarmPin_, INPUT_PULLUP); 
@@ -40,18 +40,17 @@ Security::Security(int alarmPin, int resetPin, int greenPin, int yellowPin,
   digitalWrite(redPin_,    LOW);
   
   // Put the FSM into the intial state
-  input_ =     NONE;         
+  event_ =     NONE;         
   lastState_ = GREEN;
   state_ =     GREEN;
-  pause_ = fsmTable[state_].pause;
+  timeout_ = fsmTable[state_].timeout;
   doGreen();     
 }
 
 int Security::Update()
 {
-  // States are updated only when there has been a state change and the
-  // state is not paused
-  if ((lastState_ != state_) && (pause_ == 0)){ 
+  // States are updated only when there has been a state change
+  if (lastState_ != state_) { 
     lastState_ = state_;
     switch (state_){
       case GREEN:
@@ -76,7 +75,7 @@ void Security::doGreen()
   digitalWrite(greenPin_,  HIGH);
   digitalWrite(yellowPin_, LOW);
   digitalWrite(redPin_,    LOW);
-  pause_ = fsmTable[state_].pause;
+  timeout_ = fsmTable[state_].timeout;
   startTime_ = millis();
 }
 
@@ -85,7 +84,7 @@ void Security::doYellow()
   digitalWrite(greenPin_,  LOW); 
   digitalWrite(yellowPin_, HIGH);
   digitalWrite(redPin_,    LOW);
-  pause_ = fsmTable[state_].pause;
+  timeout_ = fsmTable[state_].timeout;
   startTime_ = millis();
 }
 
@@ -94,49 +93,41 @@ void Security::doRed()
   digitalWrite(greenPin_,  LOW);
   digitalWrite(yellowPin_, LOW);
   digitalWrite(redPin_,    HIGH);
-  pause_ = fsmTable[state_].pause;
+  timeout_ = fsmTable[state_].timeout;
   startTime_ = millis();
 }
 
 void Security::getEvents(void)
 { 
+  // update the status of inputs and timeout
   int alarmEvent = digitalRead(alarmPin_);
   int resetEvent = digitalRead(resetPin_);
-  
-  if ((alarmEvent == 1) && (resetEvent == 1)){
-    input_ = NONE;
-  }
-  else if ((alarmEvent == 0) && (resetEvent == 1)){
-    input_ = ALARM;
-  }
-  else if ((alarmEvent == 1) && (resetEvent == 0)){
-    input_ = RESET;
-  }
-  else{
-    input_ = BOTH;
-  }
-  
-  // Check to see if a pause is in place for the state
-  if (pause_ > 0) {
-    // A pause condition is in place.  Compare it to the elapsed time.
+  bool timeoutEvent = true;
+  if (timeout_ > 0) {
     unsigned long elapsedTime = millis() - startTime_;
-    if (elapsedTime >= pause_){
-      // Pause is over.  Set it to 0.
-      pause_ = 0;                               
+    if (elapsedTime < timeout_){
+      timeoutEvent = false;                             
     }
+  }
+  // determine what event occurred
+  event_ = NONE;
+  if (resetEvent == 0) {
+    event_ = RESET;
+  }
+  else if ((alarmEvent == 0) && (timeoutEvent == true)){
+    event_ = ALARM;         
+  } 
+  else if (timeoutEvent == true){
+    event_ = TIMEOUT;
   }
 }
 
 void Security::getNextState(void)
 { 
-  // If the reset button is pressed (either alone or in combination with 
-  // ALARM), the state is updated and the pause set to 0.  
-  if ((input_ == RESET) || (input_ == BOTH)) {
-    pause_ = 0;
-    state_ = fsmTable[state_].nextState[input_]; 
+  if (event_ == NONE) {
+    // state remains unchanged
   }
-  // else update the state only when pause is set to 0.
-  else if (pause_ == 0) {
-    state_ = fsmTable[state_].nextState[input_];     
+  else {
+    state_ = fsmTable[state_].nextState[event_];
   }
 }
